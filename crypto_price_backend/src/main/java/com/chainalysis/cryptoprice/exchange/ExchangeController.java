4866912348726210d62957d828b3870a452070ab
Aisen Kim <aisenkim15@gmail.com>
2021-11-10 22:35:48 -0500

    [BACKEND] - removed previous getPrice method from KrakenExchangeService which called Ticker api and added error handling for getPrice method

--- crypto_price_backend/src/main/java/com/chainalysis/cryptoprice/exchange/ExchangeController.java
+++ crypto_price_backend/src/main/java/com/chainalysis/cryptoprice/exchange/ExchangeController.java
@@ -1,9 +1,11 @@
 package com.chainalysis.cryptoprice.exchange;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -21,10 +23,14 @@ public class ExchangeController {
             @RequestParam String exchange,
             @RequestParam String coinSymbol
     ) {
-        if(exchange.equals("kraken"))
-            return ResponseEntity.ok().body(krakenExchangeService.getBuySellPrice(coinSymbol));
-        else
-            return ResponseEntity.ok().body(blockchainExchangeService.getBuySellPrice(coinSymbol));
+        try {
+            if (exchange.equals("kraken"))
+                return ResponseEntity.ok().body(krakenExchangeService.getBuySellPrice(coinSymbol));
+            else
+                return ResponseEntity.ok().body(blockchainExchangeService.getBuySellPrice(coinSymbol));
+        } catch(IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("message", e.getMessage()));
+        }
     }
 
 }
--- crypto_price_backend/src/main/java/com/chainalysis/cryptoprice/exchange/KrakenExchangeService.java
+++ crypto_price_backend/src/main/java/com/chainalysis/cryptoprice/exchange/KrakenExchangeService.java
@@ -6,12 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.sql.Timestamp;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Service
@@ -20,15 +18,23 @@ public class KrakenExchangeService implements ExchangeService {
     @Autowired
     private RestTemplate restTemplate;
 
-    /**
-     * Getting price from recent 1000 trades
-     */
+//    /**
+//     * Getting price from recent 1000 trades
+//     */
     @Override
     public String getPrice(String coinSymbol) {
-        URI price = ExchangeService.buildURI("https://api.kraken.com/0/public/Trades?pair=" + coinSymbol + "USD&since=" + new Timestamp(System.currentTimeMillis()));
+        URI price = ExchangeService.buildURI("https://api.kraken.com/0/public/Trades?pair=" + coinSymbol + "US&since=" + new Timestamp(System.currentTimeMillis()));
         String result = restTemplate.getForObject(price, String.class);
 
         JSONObject resultObj = new JSONObject(result);
+
+        JSONArray errorArr = resultObj.getJSONArray("error");
+
+        // check if error exist from api call
+        if(!errorArr.isEmpty()) {
+            System.out.println("Error Message: " + errorArr.get(0));
+            throw new IllegalArgumentException(errorArr.get(0).toString());
+        }
 
         JSONArray lastSoldPriceArr = resultObj.getJSONObject("result")
                 .getJSONArray("X" + coinSymbol + "ZUSD")
@@ -36,23 +42,6 @@ public class KrakenExchangeService implements ExchangeService {
 
         return lastSoldPriceArr.getString(0);
     }
-
-    /**
-     * Getting price from Ticker
-     */
-//    @Override
-//    public String getPrice(String coinSymbol) {
-//        URI price = ExchangeService.buildURI("https://api.kraken.com/0/public/Ticker?pair=" + coinSymbol + "USD");
-//        String result = restTemplate.getForObject(price, String.class);
-//
-//        JSONObject resultObj = new JSONObject(result);
-//
-//        JSONArray lastSoldPriceArr = resultObj.getJSONObject("result")
-//                .getJSONObject("X" + coinSymbol + "ZUSD")
-//                .getJSONArray("c");
-//
-//        return lastSoldPriceArr.getString(0);
-//    }
 
     @Override
     public Map<String, String> getFees(String coinSymbol) {
