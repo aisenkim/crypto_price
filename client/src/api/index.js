9dcc833ef0f3257f56c3eea9d6b72bf8a5c080b0
Aisen Kim <aisenkim15@gmail.com>
2022-02-22 00:29:51 -0500

    added docker file

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,14 @@
-import axios from 'axios'
+import axios from "axios";
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api/v1'
-})
+  // baseURL: 'http://localhost:8080/api/v1'
+  baseURL: "http://localhost:9000/api/v1", // for deployment
+});
 
-export const getBuySellPrice = (exchange, symbol) => api.get(`/exchanges?exchange=${exchange}&coinSymbol=${symbol}`)
+export const getBuySellPrice = (exchange, symbol) =>
+  api.get(`/exchanges?exchange=${exchange}&coinSymbol=${symbol}`);
 
 const apis = {
-   getBuySellPrice
-}
+  getBuySellPrice,
+};
 
-export default apis
+export default apis;
